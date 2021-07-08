72f518c885d6098ea0404a511ee6771fd71af047
Rushikesh175 <66935903+Rushikesh175@users.noreply.github.com>
2021-07-08 11:56:33 +0530

    Assignment

---
+++ Assignment/src/Assignment/Assignment3.java
@@ -0,0 +1,23 @@
+package Assignment;
+import java.util.*;
+public class Assignment3 {
+	static StringBuffer getImage(String s1)
+    {
+            StringBuffer sb=new StringBuffer(s1);
+            StringBuffer s2=sb.reverse();
+            return s2;
+        
+    }
+  public static void main(String[] args) {
+    String s1;
+        Scanner scan=new Scanner(System.in);
+        s1=scan.nextLine();
+        StringBuffer s3=getImage(s1);
+        System.out.println(s1+"|"+s3);
+      
+  }
+}
+
+	  
+
+
